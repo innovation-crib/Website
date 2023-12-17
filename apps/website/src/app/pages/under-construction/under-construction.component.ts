1250d690f8b2d36967429391444eb6f099d5a3fd
Lisa Scheers <lisa@scheers.tech>
2023-12-17 01:56:51 +0100

    aaaa

---
+++ apps/website/src/app/pages/under-construction/under-construction.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'innovation-crib-under-construction',
+  templateUrl: 'under-construction.component.html',
+  standalone: true,
+})
+export class UnderConstructionComponent {}
